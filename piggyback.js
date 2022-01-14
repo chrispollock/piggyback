5f100639c32dbaa6a1cc1e05d923ffaf0026f223
Chris Pollock <chris@chrispollock.co.uk>
2022-01-14 17:28:50 +0000

    Initial commit

---
+++ piggyback.js
@@ -0,0 +1,32 @@
+async function getRecords( endpoint ) {
+    return ( await fetch( `https://dns.google/resolve?name=${ endpoint }&type=TXT` ) ).json();
+}
+
+function formatRecordsToHTML( { Answer } ) {
+    let HTMLObject = {};
+
+    Answer.map( entry => entry.data )
+          .filter( entry => entry.match( /PNS_(\d+):(.*)/ ) )
+          .forEach( entry => {
+              entry.substring( 4 )
+                   .split( /:(.+)/ )
+                   .reduce( ( id, value ) => {
+                       HTMLObject[ id ] = value;
+                   } );
+          } );
+
+    return Object.values( HTMLObject ).join( '' );
+}
+
+function setDOM( payload ) {
+    document.body.innerHTML = payload;
+}
+
+const endpoint = 'pns.test.2.vow.systems';
+
+getRecords( endpoint ).then( records => {
+    let html = formatRecordsToHTML( records );
+
+    setDOM( html );
+} );
+    
